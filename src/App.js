08c5b4f9acae915ed68918c2d0e3e11cb94c0683
priyam125 <sinha.priyam002@gmail.com>
2023-11-20 17:15:22 +0530

    background added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Dashboard from "./pages/Dashboard";
+import "typeface-inter";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ fontFamily: "'Inter', sans-serif" }} className="App">
       <Dashboard />
     </div>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import BellIcon from "../assets/Bell.svg";
 
 const Dashboard = () => {
   return (
     <div id="dashboard-container" className="flex w-screen h-screen">
       <div
         id="gradient-dashboard"
-        className="flex flex-col w-3/5 bg-blue-950"
+        className="flex flex-col w-[55%] bg-blue-950 p-16"
         style={{
           background:
-            "linear-gradient(45deg, #1FA911 5.01%, #2F15D0 70%, #000000 0%)",
+            "radial-gradient(at 0% 100%, rgba(31,169,17,6) 12%, rgba(47,21,208,1) 55%, rgba(0,0,0,1) 90%)",
         }}
       >
-        Dashboard
+        <div id="dashboard-body" className="flex flex-col mt-5">
+          <div id="dashboard-notification" className="flex w-[40%]">
+            <div
+              id="notification-info"
+              className="flex flex-col items-start text-white space-y-2"
+            >
+              <img src={BellIcon} alt="Notification Bell" />
+              <div className="text-3xl font-medium">
+                Get notified when a highly correlated whale makes a move
+              </div>
+              <div className="opacity-70">
+                Find out when a certain whale moves more than any preset amount
+                on-chain or when a dormant whale you care about becomes active.
+              </div>
+            </div>
+          </div>
+          <div id="dashboard-whales"></div>
+        </div>
+        <div id="dashboard-footer" className="flex"></div>
       </div>
       <div id="signUp-container">SignUp</div>
     </div>
